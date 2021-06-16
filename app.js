968f2a682a92b60006abdb8c17cb340321d0eda7
omar1779 <josemyclans@gmail.com>
2021-06-15 23:06:55 -0500

    añadi scrollreveal con JS

---
+++ app.js
@@ -0,0 +1,14 @@
+ScrollReveal().reveal('.title');
+ScrollReveal().reveal('.a1',{ delay: 200 });
+ScrollReveal().reveal('.a2',{ delay: 500 });
+ScrollReveal().reveal('.image-menu',{ delay: 700 });
+ScrollReveal().reveal('.title2',{ delay: 900});
+
+ScrollReveal().reveal('.carousel');
+ScrollReveal().reveal('.carousel-item',{ delay: 200});
+ScrollReveal().reveal('.container-aboute');
+ScrollReveal().reveal('.container-description');
+ScrollReveal().reveal('.img-one',{ delay: 500});
+ScrollReveal().reveal('.img-two',{ delay: 900});
+ScrollReveal().reveal('.img-three',{ delay: 1200});
+ScrollReveal().reveal('.img-four',{ delay: 1500});
