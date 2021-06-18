45f9f54be38b928b5591584ee5872c8cc550602a
omar1779 <josemyclans@gmail.com>
2021-06-18 01:07:02 -0500

    version 3 , cambios visuales

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 ScrollReveal().reveal('.title');
-ScrollReveal().reveal('.a1',{ delay: 200 });
-ScrollReveal().reveal('.a2',{ delay: 500 });
-ScrollReveal().reveal('.image-menu',{ delay: 700 });
-ScrollReveal().reveal('.title2',{ delay: 900});
+ScrollReveal().reveal('.a1',{ delay: 400 });
+ScrollReveal().reveal('.a2',{ delay: 700 });
+ScrollReveal().reveal('.image-menu',{ delay: 900 });
+ScrollReveal().reveal('.title2',{ delay: 1200});
 
 ScrollReveal().reveal('.carousel');
 ScrollReveal().reveal('.carousel-item',{ delay: 200});
 ScrollReveal().reveal('.container-aboute');
+ScrollReveal().reveal('.title-about');
 ScrollReveal().reveal('.container-description');
-ScrollReveal().reveal('.img-one',{ delay: 500});
-ScrollReveal().reveal('.img-two',{ delay: 900});
-ScrollReveal().reveal('.img-three',{ delay: 1200});
-ScrollReveal().reveal('.img-four',{ delay: 1500});
+ScrollReveal().reveal('.img-one',{ delay: 900});
+ScrollReveal().reveal('.img-two',{ delay: 1200});
+ScrollReveal().reveal('.img-three',{ delay: 1500});
+ScrollReveal().reveal('.img-four',{ delay: 1800});
