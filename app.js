26d7e3b09dbe5cc6f9a27f3f1bf654ea27e2285e
omar1779 <josemyclans@gmail.com>
2021-06-20 00:07:51 -0500

    cambio de fondo y diseño

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+//scroll Reveal//
 ScrollReveal().reveal('.title');
 ScrollReveal().reveal('.a1',{ delay: 400 });
 ScrollReveal().reveal('.a2',{ delay: 700 });
 ScrollReveal().reveal('.image-menu',{ delay: 900 });
 ScrollReveal().reveal('.title2',{ delay: 1200});
-
 ScrollReveal().reveal('.carousel');
 ScrollReveal().reveal('.carousel-item',{ delay: 200});
 ScrollReveal().reveal('.container-aboute');
 ScrollReveal().reveal('.title-about');
 ScrollReveal().reveal('.container-description');
+ScrollReveal().reveal('.container-language');
 ScrollReveal().reveal('.img-one',{ delay: 900});
 ScrollReveal().reveal('.img-two',{ delay: 1200});
 ScrollReveal().reveal('.img-three',{ delay: 1500});
@@ -19,3 +20,17 @@ ScrollReveal().reveal('.formulario',{ delay: 600});
 ScrollReveal().reveal('.c-input',{ delay: 800});
 ScrollReveal().reveal('.c-input2',{ delay: 1000});
 ScrollReveal().reveal('.message',{ delay: 1200});
+
+//recoleccion de datos del formulario ejecutado por consola mediante una funcion//
+
+function captura(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    let formulario ="hola soy: "+ name+
+                    " mi email es el siguiente: "+ email+
+                    " este mensaje es para ti: "+message;
+    console.log(formulario);
+}
+
