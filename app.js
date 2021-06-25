1cccb0dfc95faaa1a706d844ba02546d93b28012
omar1779 <josemyclans@gmail.com>
2021-06-25 02:47:38 -0500

    añadi fontawesome y elimine el form , al igual que su javascript

--- app.js
+++ app.js
@@ -10,27 +10,14 @@ ScrollReveal().reveal('.container-aboute');
 ScrollReveal().reveal('.title-about');
 ScrollReveal().reveal('.container-description');
 ScrollReveal().reveal('.container-language');
-ScrollReveal().reveal('.img-one',{ delay: 900});
-ScrollReveal().reveal('.img-two',{ delay: 1200});
-ScrollReveal().reveal('.img-three',{ delay: 1500});
-ScrollReveal().reveal('.img-four',{ delay: 1800});
-ScrollReveal().reveal('.container-form',{ delay: 200});
-ScrollReveal().reveal('.title-form',{ delay: 400});
-ScrollReveal().reveal('.formulario',{ delay: 600});
-ScrollReveal().reveal('.c-input',{ delay: 800});
-ScrollReveal().reveal('.c-input2',{ delay: 1000});
-ScrollReveal().reveal('.message',{ delay: 1200});
+ScrollReveal().reveal('.fa-css3-alt',{ delay: 900});
+ScrollReveal().reveal('.fa-sass',{ delay: 1200});
+ScrollReveal().reveal('.fa-js-square',{ delay: 1500});
+ScrollReveal().reveal('.fa-html5',{ delay: 1800});
+ScrollReveal().reveal('.fa-react',{ delay: 2300});
+ScrollReveal().reveal('.fa-bootstrap',{ delay: 1800});
+ScrollReveal().reveal('.skills');
 
 //recoleccion de datos del formulario ejecutado por consola mediante una funcion//
 
-function captura(){
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let message = document.getElementById("message").value;
-
-    let formulario ="hola soy: "+ name+
-                    " mi email es el siguiente: "+ email+
-                    " este mensaje es para ti: "+message;
-    console.log(formulario);
-}
 
