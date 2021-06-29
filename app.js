7e6ed5e00261d26535d2eac2d73150c16bc69177
omar1779 <josemyclans@gmail.com>
2021-06-29 01:14:38 -0500

    proyectos y contactos

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ ScrollReveal().reveal('.carousel');
 ScrollReveal().reveal('.carousel-item',{ delay: 200});
 ScrollReveal().reveal('.container-aboute');
 ScrollReveal().reveal('.title-about');
-ScrollReveal().reveal('.container-description');
+ScrollReveal().reveal('.container-description',{ delay: 400});
+ScrollReveal().reveal('.description',{ delay: 700});
 ScrollReveal().reveal('.container-language');
 ScrollReveal().reveal('.fa-css3-alt',{ delay: 900});
 ScrollReveal().reveal('.fa-sass',{ delay: 1200});
@@ -17,6 +18,7 @@ ScrollReveal().reveal('.fa-html5',{ delay: 1800});
 ScrollReveal().reveal('.fa-react',{ delay: 2300});
 ScrollReveal().reveal('.fa-bootstrap',{ delay: 1800});
 ScrollReveal().reveal('.skills');
+ScrollReveal().reveal('.carousel-item',{ delay: 800});
 
 //recoleccion de datos del formulario ejecutado por consola mediante una funcion//
 
