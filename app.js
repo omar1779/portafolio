30a3b585feb7f2672217642c55fbf6e3d33faea3
omar1779 <josemyclans@gmail.com>
2021-07-08 14:53:34 -0500

    ultimos ajustes

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ ScrollReveal().reveal('.fa-react',{ delay: 2300});
 ScrollReveal().reveal('.fa-bootstrap',{ delay: 1800});
 ScrollReveal().reveal('.skills');
 ScrollReveal().reveal('.carousel-item',{ delay: 800});
+ScrollReveal().reveal('.container-contact',{ delay: 800});
+ScrollReveal().reveal('.fa-twitter',{ delay: 1000});
+ScrollReveal().reveal('.fa-linkedin-in',{ delay: 1200});
+ScrollReveal().reveal('.fa-instagram',{ delay: 1400});
+ScrollReveal().reveal('.fa-github',{ delay: 1600});
+ScrollReveal().reveal('.title-footer',{ delay: 600});
+ScrollReveal().reveal('.enlace-home',{ delay: 1200});
 
 //recoleccion de datos del formulario ejecutado por consola mediante una funcion//
 
